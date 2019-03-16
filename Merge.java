c59be223381f32146df89503853fb4d756af751e
bertw2002 <awan00@stuy.edu>
2019-03-16 16:58:55 -0400

    finished untested merge code

--- Merge.java
+++ Merge.java
@@ -6,13 +6,35 @@ public class Merge{
   private static void mergesort(int[] data, int[]temp, int lo, int hi){
 
   }
+  //helper for mergesort
   private static void merge(int[] data1, int[] data2){
     int[] newData = new int[data1.length + data2.length];
+    int counter = 0;
     int count1 = 0;
     int count2 = 0;
     while (count1 < data1.length && count2 < data2.length){
-      if (data1[count1] > data2[count2]){
-        
+      if (data1[count1] < data2[count2]){
+        newData[counter] = data1[count1];
+        count1++;
+      }else{
+        newData[counter] = data2[count2];
+        count2++;
+      }
+      counter++;
+    }
+
+    if (count1 < data1.length){
+      while (count1 < data1.length){
+        newData[counter] = data1[count1];
+        counter++;
+        count1++;
+      }
+    }
+    if (count2 < data2.length){
+      while (count2 < data2.length){
+        newData[counter] = data2[count2];
+        counter++;
+        count2++;
       }
     }
   }
