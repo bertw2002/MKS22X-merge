886f4dcd50a88683a3776122dac60e7728941652
bertw2002 <awan00@stuy.edu>
2019-03-16 18:21:31 -0400

    finished code. Haven't tested

--- Merge.java
+++ Merge.java
@@ -1,48 +1,64 @@
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[] data){
-
+    mergesort(data, 0, data.length - 1);
   }
-  private static void mergesort(int[] data, int[]temp, int lo, int hi){
-
+  private static void mergesort(int[] data, int lo, int hi){
+    if (lo >= hi){
+      return;
+    }
+    int middle = (hi-lo) / 2 + lo;
+    //sort first and second halves.
+    mergesort(data, lo, middle);
+    mergesort(data, middle + 1, hi);
+    merge(data, lo, middle, hi);
   }
   //helper for mergesort
-  public static int[] merge(int[] data1, int[] data2){
-    int[] newData = new int[data1.length + data2.length];
+  private static void merge(int[] data, int lo, int med, int hi){
+    int[] data1 = new int[med - lo + 1];
+    //dont add one because it the arrary starts at med + 1
+    int[] data2 = new int[hi - med];
     int counter = 0;
     int count1 = 0;
     int count2 = 0;
+    //intialize temp arrays
+    for (int x = 0; x < (med - lo + 1); x++){
+      data1[x] = data[lo + x];
+    }
+    for (int x = 0; x < (hi - med); x++){
+      data2[x] = data[med + 1 + x];
+    }
+    //loops thru initially to find smaller values.
     while (count1 < data1.length && count2 < data2.length){
       if (data1[count1] < data2[count2]){
-        newData[counter] = data1[count1];
+        data[counter] = data1[count1];
         count1++;
       }else{
-        newData[counter] = data2[count2];
+        data[counter] = data2[count2];
         count2++;
       }
       counter++;
     }
-
+    //cleans up remaining values of data1 or data2.
     while (count1 < data1.length){
-      newData[counter] = data1[count1];
+      data[counter] = data1[count1];
       counter++;
       count1++;
     }
     while (count2 < data2.length){
-      newData[counter] = data2[count2];
+      data[counter] = data2[count2];
       counter++;
       count2++;
     }
-
-    return newData;
   }
   public static void main(String[] args){
+    /*
     int[] data1 = {0, 3, 4, 5, 10};
     int[] data2 = {1, 2, 6, 15, 100};
     System.out.println("\n" + "Testing Merge" + "\n");
-    int[] newArr = merge(data1, data2);
+    merge(data1, data2);
     for (int x = 0; x < newArr.length; x++){
       System.out.println(newArr[x]);
-    }
+    }*/
   }
 }
